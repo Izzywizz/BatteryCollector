4a3bc12eca644153ee33167c57783e0312967e9e
Isfand <izzywhizzzz@gmail.com>
2019-05-30 13:32:07 +0100

    Added getters and setters for an active pickup item

--- Source/BatteryCollector/Pickup.cpp
+++ Source/BatteryCollector/Pickup.cpp
@@ -10,6 +10,7 @@ APickup::APickup()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	// All pickups start active
 	bIsActive = true;
 	// Create the static mesh component
 	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
@@ -30,3 +31,13 @@ void APickup::Tick(float DeltaTime)
 
 }
 
+// Returns active state
+bool APickup::IsActive()
+{
+	return bIsActive;
+}
+//Changes active state
+void APickup::SetActive(bool NewPickupState)
+{
+	bIsActive = NewPickupState;
+}
--- Source/BatteryCollector/Pickup.h
+++ Source/BatteryCollector/Pickup.h
@@ -28,6 +28,14 @@ public:
 	// Return the mesh for the pickup
 	FORCEINLINE class UStaticMeshComponent* GetMesh() const {return PickupMesh;};
 
+	// Return whether or not the pickup is active
+	UFUNCTION(BlueprintPure, Category = "Pickup")
+	bool IsActive();
+
+	// Allows other classes to safely change whether or not the pickup is active
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	void SetActive(bool NewPickupState);
+
 private:
 // Static mesh ro represent the pickup in the level
 UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
